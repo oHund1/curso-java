f75d026dd8b8481d93c4a62117580877f5023ded
oHund1 <caeduzim@gmail.com>
2022-08-10 18:53:53 -0300

    Exercício 07

---
+++ src/br/com/carlos/exercicio07/Exercicio07.java
@@ -0,0 +1,20 @@
+package br.com.carlos.exercicio07;
+
+import java.util.Scanner;
+
+public class Exercicio07 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Ola");
+        System.out.print("Digite a medida do lado do quadrado: ");
+        double lado = Double.parseDouble(String.valueOf(leitor.nextDouble()));
+        double area = lado*lado;
+        double dobro = area*2;
+
+        System.out.println("Area do quadrado e: " + area);
+        System.out.println("O dobro da area e: " + dobro);
+
+
+    }
+}
