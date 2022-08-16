1903329896478dbb564f8e392070e035ad4234a6
oHund1 <caeduzim@gmail.com>
2022-08-16 19:51:03 -0300

    Exercício 16

--- src/br/com/carlos/exercicio16/Exercicio16.java
+++ src/br/com/carlos/exercicio16/Exercicio16.java
@@ -7,5 +7,16 @@ public class Exercicio16 {
         Scanner leitor = new Scanner(System.in);
 
         System.out.println("Ola!");
+
+        System.out.print("Digite a metragem a ser pintada em metros: ");
+        double area = leitor.nextDouble();
+        double litros = area / 3;
+        double latas = litros / 18;
+
+        int quantidadeLatas = (int) Math.ceil(latas);
+        int precoFinal = (quantidadeLatas * 80);
+
+        System.out.println("A quantidade de latas necessarias sera de: " + quantidadeLatas);
+        System.out.println("O preco da compra sera de: " + precoFinal + " reais.");
     }
 }
